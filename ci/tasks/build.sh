5e56ceebef46255c1b3add2f4ddec9f4752352c5
Colin Simmons <colin.simmons@engineerbetter.com>
2020-02-06 11:31:20 +0000

    We don't need to move files onto the GOPATH to build now that we have modules

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # shellcheck disable=SC1091
 source control-tower/ci/tasks/lib/set-flags.sh
@@ -12,16 +12,12 @@ else
   version="TESTVERSION"
 fi
 
-mkdir -p "$GOPATH/src/github.com/EngineerBetter/control-tower"
-mkdir -p "$GOPATH/src/github.com/EngineerBetter/control-tower-ops"
-mv control-tower/* "$GOPATH/src/github.com/EngineerBetter/control-tower"
-mv control-tower-ops/* "$GOPATH/src/github.com/EngineerBetter/control-tower-ops"
-cd "$GOPATH/src/github.com/EngineerBetter/control-tower" || exit 1
-
 GOOS=linux go get -u github.com/kevinburke/go-bindata/...
 
+cd control-tower || exit 1
+
 grep -lr --include=*.go --exclude-dir=vendor "go:generate go-bindata" . | xargs -I {} go generate {}
-go build -ldflags "
-  -X main.ControlTowerVersion=$version
+GO111MODULE=on go build -mod=vendor -ldflags "
   -X github.com/EngineerBetter/control-tower/fly.ControlTowerVersion=$version
+  -X main.ControlTowerVersion=$version
 " -o "$build_dir/$OUTPUT_FILE"
