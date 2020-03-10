4508d82c78cc2a1280c85f6326622741468ac69d
Tom Denley <t.denley@strategic-blue.com>
2020-03-10 20:17:15 +0000

    Add icons to resources in concourse pipelines

--- fly/aws_pipeline_test.go
+++ fly/aws_pipeline_test.go
@@ -14,12 +14,14 @@ var _ = Describe("AWSPipeline", func() {
 resources:
 - name: control-tower-release
   type: github-release
+  icon: github
   source:
     user: engineerbetter
     repository: control-tower
     pre_release: true
 - name: every-day
   type: time
+  icon: clock
   source: {interval: 24h}
 
 jobs:
