828e88a5633b4bc550e8cae2d2a328bf86f9b87b
Irbe Krumina <irbekrm@gmail.com>
2020-04-15 09:05:48 +0100

    Explicitly passes multiple tag filters in AWS system-test-options-and-destroy

--- ci/tasks/lib/tags.sh
+++ ci/tasks/lib/tags.sh
@@ -18,7 +18,9 @@ function assertTagsSet() {
   then
     tagged_instances=$(aws ec2 --region us-east-1 \
         describe-instances \
-        --filters="Name=tag:unique-tag Values=special-value Name=tag:yet-another-tag Values=some-value Name=tag:control-tower-project Values=$deployment" \
+        --filters="Name=tag:unique-tag,Values=special-value" \
+        --filters="Name=tag:yet-another-tag,Values=some-value" \
+        --filters="Name=tag:control-tower-project,Values=$deployment" \
         | jq -r '.Reservations | length')
   fi
 
