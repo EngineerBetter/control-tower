dce2088a63e67a6098312cb7f50fc54366fac32b
Will Gant <w_gant@yahoo.co.uk>
2020-04-21 16:28:51 +0200

    Satisfy the linter

--- ci/tasks/lib/wait-for-lock.sh
+++ ci/tasks/lib/wait-for-lock.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 function waitForBoshLock() {
-  for num in {1..60}; do
+  for i in {1..60}; do
     bosh_locks=$(bosh locks --json | jq '.Tables[0].Rows[] | select(.resource=="concourse")') 
     if [[ -n "${bosh_locks}" ]]; then
-        echo "Concourse still has a BOSH lock, waiting another 60 seconds..."
+        echo "Attempt ${i}: Concourse still has a BOSH lock, waiting another 60 seconds..."
         sleep 60
     else
         break
