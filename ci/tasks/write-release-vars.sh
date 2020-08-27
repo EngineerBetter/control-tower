8d20b2d285a333f1c307d0c8d95a20fe3d605252
Andy Paine <andy.paine@engineerbetter.com>
2020-08-27 12:38:54 +0100

    Don't publish versions of releases we don't use
    These releases are no longer needed and were just producing empty
    versions in release notes

--- ci/tasks/write-release-vars.sh
+++ ci/tasks/write-release-vars.sh
@@ -15,14 +15,10 @@ pushd control-tower-ops
   deployment_concourse_release_version=$(jq -r '.[] | select(.value.name? == "concourse") | .value.version' ops/versions-aws.json)
   deployment_credhub_release_url=$(      jq -r '.[] | select(.value.name? == "credhub") | .value.url' ops/versions-aws.json)
   deployment_credhub_release_version=$(  jq -r '.[] | select(.value.name? == "credhub") | .value.version' ops/versions-aws.json)
-  deployment_garden_release_url=$(       jq -r '.[] | select(.value.name? == "garden-runc") | .value.url' ops/versions-aws.json)
-  deployment_garden_release_version=$(   jq -r '.[] | select(.value.name? == "garden-runc") | .value.version' ops/versions-aws.json)
   deployment_grafana_release_url=$(      jq -r '.[] | select(.value.name? == "grafana") | .value.url' ops/versions-aws.json)
   deployment_grafana_release_version=$(  jq -r '.[] | select(.value.name? == "grafana") | .value.version' ops/versions-aws.json)
   deployment_influxdb_release_url=$(     jq -r '.[] | select(.value.name? == "influxdb") | .value.url' ops/versions-aws.json)
   deployment_influxdb_release_version=$( jq -r '.[] | select(.value.name? == "influxdb") | .value.version' ops/versions-aws.json)
-  deployment_riemann_release_url=$(      jq -r '.[] | select(.value.name? == "riemann") | .value.url' ops/versions-aws.json)
-  deployment_riemann_release_version=$(  jq -r '.[] | select(.value.name? == "riemann") | .value.version' ops/versions-aws.json)
   deployment_stemcell_version=$(         jq -r '.[] | select(.path == "/stemcells/alias=xenial/version") | .value' ops/versions-aws.json)
   deployment_uaa_release_url=$(          jq -r '.[] | select(.value.name? == "uaa") | .value.url' ops/versions-aws.json)
   deployment_uaa_release_version=$(      jq -r '.[] | select(.value.name? == "uaa") | .value.version' ops/versions-aws.json)
@@ -40,14 +36,10 @@ pushd control-tower-ops
   deployment_concourse_release_version_gcp=$(jq -r '.[] | select(.value.name? == "concourse") | .value.version' ops/versions-gcp.json)
   deployment_credhub_release_url_gcp=$(      jq -r '.[] | select(.value.name? == "credhub") | .value.url' ops/versions-gcp.json)
   deployment_credhub_release_version_gcp=$(  jq -r '.[] | select(.value.name? == "credhub") | .value.version' ops/versions-gcp.json)
-  deployment_garden_release_url_gcp=$(       jq -r '.[] | select(.value.name? == "garden-runc") | .value.url' ops/versions-gcp.json)
-  deployment_garden_release_version_gcp=$(   jq -r '.[] | select(.value.name? == "garden-runc") | .value.version' ops/versions-gcp.json)
   deployment_grafana_release_url_gcp=$(      jq -r '.[] | select(.value.name? == "grafana") | .value.url' ops/versions-gcp.json)
   deployment_grafana_release_version_gcp=$(  jq -r '.[] | select(.value.name? == "grafana") | .value.version' ops/versions-gcp.json)
   deployment_influxdb_release_url_gcp=$(     jq -r '.[] | select(.value.name? == "influxdb") | .value.url' ops/versions-gcp.json)
   deployment_influxdb_release_version_gcp=$( jq -r '.[] | select(.value.name? == "influxdb") | .value.version' ops/versions-gcp.json)
-  deployment_riemann_release_url_gcp=$(      jq -r '.[] | select(.value.name? == "riemann") | .value.url' ops/versions-gcp.json)
-  deployment_riemann_release_version_gcp=$(  jq -r '.[] | select(.value.name? == "riemann") | .value.version' ops/versions-gcp.json)
   deployment_stemcell_version_gcp=$(         jq -r '.[] | select(.path == "/stemcells/alias=xenial/version") | .value' ops/versions-gcp.json)
   deployment_uaa_release_url_gcp=$(          jq -r '.[] | select(.value.name? == "uaa") | .value.url' ops/versions-gcp.json)
   deployment_uaa_release_version_gcp=$(      jq -r '.[] | select(.value.name? == "uaa") | .value.version' ops/versions-gcp.json)
@@ -79,14 +71,12 @@ Deploys:
 - Concourse VM stemcell bosh-aws-xen-hvm-ubuntu-xenial-go_agent $deployment_stemcell_version
 - Director stemcell     bosh-aws-xen-hvm-ubuntu-xenial-go_agent $director_stemcell_version
 - Concourse [$deployment_concourse_release_version]($deployment_concourse_release_url)
-- Garden RunC [$deployment_garden_release_version]($deployment_garden_release_url)
 - BOSH [$director_bosh_release_version]($director_bosh_release_url)
 - BOSH AWS CPI [$director_bosh_cpi_release_version]($director_bosh_cpi_release_url)
 - BPM [$director_bpm_release_version]($director_bpm_release_url)
 - Credhub [$deployment_credhub_release_version]($deployment_credhub_release_url)
 - Grafana [$deployment_grafana_release_version]($deployment_grafana_release_url)
 - InfluxDB [$deployment_influxdb_release_version]($deployment_influxdb_release_url)
-- Riemann [$deployment_riemann_release_version]($deployment_riemann_release_url)
 - UAA [$deployment_uaa_release_version]($deployment_uaa_release_url)
 - BOSH CLI $bin_bosh_cli_version
 - Terraform $bin_terraform_version
@@ -96,14 +86,12 @@ Deploys:
 - Concourse VM stemcell bosh-google-kvm-ubuntu-xenial-go_agent $deployment_stemcell_version_gcp
 - Director stemcell     bosh-google-kvm-ubuntu-xenial-go_agent $director_stemcell_version_gcp
 - Concourse [$deployment_concourse_release_version_gcp]($deployment_concourse_release_url_gcp)
-- Garden RunC [$deployment_garden_release_version_gcp]($deployment_garden_release_url_gcp)
 - BOSH [$director_bosh_release_version_gcp]($director_bosh_release_url_gcp)
 - BOSH GCP CPI [$director_bosh_cpi_release_version_gcp]($director_bosh_cpi_release_url_gcp)
 - BPM [$director_bpm_release_version_gcp]($director_bpm_release_url_gcp)
 - Credhub [$deployment_credhub_release_version_gcp]($deployment_credhub_release_url_gcp)
 - Grafana [$deployment_grafana_release_version_gcp]($deployment_grafana_release_url_gcp)
 - InfluxDB [$deployment_influxdb_release_version_gcp]($deployment_influxdb_release_url_gcp)
-- Riemann [$deployment_riemann_release_version_gcp]($deployment_riemann_release_url_gcp)
 - UAA [$deployment_uaa_release_version_gcp]($deployment_uaa_release_url_gcp)
 - BOSH CLI $bin_bosh_cli_version_gcp
 - Terraform $bin_terraform_version_gcp
