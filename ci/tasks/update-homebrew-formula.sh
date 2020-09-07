0a2021ce3ade29c57eb3b4957702c50ab32d40b6
Will Gant <w_gant@yahoo.co.uk>
2020-09-07 17:02:47 +0200

    Add missing double quote

--- ci/tasks/update-homebrew-formula.sh
+++ ci/tasks/update-homebrew-formula.sh
@@ -14,7 +14,7 @@ class ControlTower < Formula
   desc "Deploy and operate Concourse CI in a single command"
   homepage "https://www.engineerbetter.com"
   license "Apache-2.0"
-  version "${version}
+  version "${version}"
 
   if OS.mac?
     url "https://github.com/EngineerBetter/control-tower/releases/download/#{version}/control-tower-darwin-amd64"
