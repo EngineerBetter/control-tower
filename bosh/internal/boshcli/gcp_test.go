998a6a9f235dba7f1039c790dc6a269e98281166
Colin Simmons <5785492+crsimmons@users.noreply.github.com>
2021-05-14 22:15:56 +0100

    remove references to xenial in the tests

--- bosh/internal/boshcli/gcp_test.go
+++ bosh/internal/boshcli/gcp_test.go
@@ -1,11 +1,12 @@
 package boshcli
 
 import (
-	"github.com/EngineerBetter/control-tower/resource"
-	"github.com/onsi/gomega/format"
 	"io/ioutil"
 	"testing"
 	"text/template"
+
+	"github.com/EngineerBetter/control-tower/resource"
+	"github.com/onsi/gomega/format"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -76,7 +77,7 @@ func TestGCPEnvironment_ConfigureConcourseStemcell(t *testing.T) {
 	}{
 		{
 			name:    "parse versions and provide a valid stemcell url",
-			want:    "https://s3.amazonaws.com/bosh-gce-light-stemcells/5/light-bosh-stemcell-5-google-kvm-ubuntu-xenial-go_agent.tgz",
+			want:    "https://s3.amazonaws.com/bosh-gce-light-stemcells/5/light-bosh-stemcell-5-google-kvm-ubuntu-bionic-go_agent.tgz",
 			wantErr: false,
 			fixture: "stemcell_version",
 		},
