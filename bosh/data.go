4b71516fcd51afae83033065d44d05b5fc531987
Richard Pryce <rich_pryce@yahoo.co.uk>
2021-08-26 13:55:10 +0100

    Use embed where there is no relative path

--- bosh/data.go
+++ bosh/data.go
@@ -1,28 +1,47 @@
 package bosh
 
-const concourseManifestFilename = "concourse.yml"
-const credsFilename = "concourse-creds.yml"
-const concourseDeploymentName = "concourse"
-const concourseVersionsFilename = "versions.json"
-const concourseSHAsFilename = "shas.json"
-const concourseGrafanaFilename = "grafana_dashboard.yml"
-const concourseBitBucketAuthFilename = "bitbucket-auth.yml"
-const concourseGitHubAuthFilename = "github-auth.yml"
-const concourseMicrosoftAuthFilename = "microsoft-auth.yml"
-const concourseEphemeralWorkersFilename = "ephemeral_workers.yml"
-const extraTagsFilename = "extra_tags.yml"
-const uaaCertFilename = "uaa-cert.yml"
+import _ "embed"
+
+const (
+	credsFilename                     = "concourse-creds.yml"
+	concourseManifestFilename         = "concourse.yml"
+	concourseDeploymentName           = "concourse"
+	concourseVersionsFilename         = "versions.json"
+	concourseSHAsFilename             = "shas.json"
+	concourseGrafanaFilename          = "grafana_dashboard.yml"
+	concourseBitBucketAuthFilename    = "bitbucket-auth.yml"
+	concourseGitHubAuthFilename       = "github-auth.yml"
+	concourseMicrosoftAuthFilename    = "microsoft-auth.yml"
+	concourseEphemeralWorkersFilename = "ephemeral_workers.yml"
+	extraTagsFilename                 = "extra_tags.yml"
+	uaaCertFilename                   = "uaa-cert.yml"
+)
 
 //go:generate go-bindata -pkg $GOPACKAGE -ignore \.git assets/... ../../control-tower-ops/... ../resource/assets/...
-var concourseGrafana = MustAsset("assets/grafana_dashboard.yml")
-var concourseBitBucketAuth = MustAsset("assets/ops/bitbucket-auth.yml")
-var concourseGitHubAuth = MustAsset("assets/ops/github-auth.yml")
-var concourseMicrosoftAuth = MustAsset("assets/ops/microsoft-auth.yml")
-var concourseEphemeralWorkers = MustAsset("assets/ops/ephemeral_workers.yml")
-var extraTags = MustAsset("assets/ops/extra_tags.yml")
-var concourseManifestContents = MustAsset("../../control-tower-ops/manifest.yml")
-var awsConcourseVersions = MustAsset("../../control-tower-ops/ops/versions-aws.json")
-var awsConcourseSHAs = MustAsset("../../control-tower-ops/ops/shas-aws.json")
-var gcpConcourseVersions = MustAsset("../../control-tower-ops/ops/versions-gcp.json")
-var gcpConcourseSHAs = MustAsset("../../control-tower-ops/ops/shas-gcp.json")
-var uaaCert = MustAsset("../resource/assets/gcp/uaa-cert.yml")
+
+var (
+	//go:embed assets/grafana_dashboard.yml
+	concourseGrafana []byte
+
+	//go:embed assets/ops/bitbucket-auth.yml
+	concourseBitBucketAuth []byte
+
+	//go:embed assets/ops/github-auth.yml
+	concourseGitHubAuth []byte
+
+	//go:embed assets/ops/microsoft-auth.yml
+	concourseMicrosoftAuth []byte
+
+	//go:embed assets/ops/ephemeral_workers.yml
+	concourseEphemeralWorkers []byte
+
+	//go:embed assets/ops/extra_tags.yml
+	extraTags []byte
+
+	concourseManifestContents = MustAsset("../../control-tower-ops/manifest.yml")
+	awsConcourseVersions      = MustAsset("../../control-tower-ops/ops/versions-aws.json")
+	awsConcourseSHAs          = MustAsset("../../control-tower-ops/ops/shas-aws.json")
+	gcpConcourseVersions      = MustAsset("../../control-tower-ops/ops/versions-gcp.json")
+	gcpConcourseSHAs          = MustAsset("../../control-tower-ops/ops/shas-gcp.json")
+	uaaCert                   = MustAsset("../resource/assets/gcp/uaa-cert.yml")
+)
--- resource/package.go
+++ resource/package.go
@@ -22,26 +22,44 @@ var (
 	AWSCPIOps string
 
 	// ExternalIPOps statically defines external-ip.yml contents
-	AWSExternalIPOps = file.MustAssetString("assets/aws/external-ip.yml")
+	//go:embed assets/aws/external-ip.yml
+	AWSExternalIPOps string
+
 	// AWSDirectorCustomOps statically defines custom-ops.yml contents
-	AWSDirectorCustomOps = file.MustAssetString("assets/aws/custom-ops.yml")
+	//go:embed assets/aws/custom-ops.yml
+	AWSDirectorCustomOps string
+
 	// AWSBlobstoreOps defines s3-blobstore-ops.yml contents
-	AWSBlobstoreOps = file.MustAssetString("assets/aws/s3-blobstore-ops.yml")
+	//go:embed assets/aws/s3-blobstore-ops.yml
+	AWSBlobstoreOps string
+
 	// GCPDirectorCloudConfig statically defines gcp cloud-config.yml
-	GCPDirectorCloudConfig = file.MustAssetString("assets/gcp/cloud-config.yml")
+	//go:embed assets/gcp/cloud-config.yml
+	GCPDirectorCloudConfig string
+
 	// GCPCPIOps statically defines gcp-cpi.yml contents
-	GCPCPIOps = file.MustAssetString("assets/gcp/cpi.yml")
+	//go:embed assets/gcp/cpi.yml
+	GCPCPIOps string
+
 	// GCPExternalIPOps statically defines external-ip.yml contents
-	GCPExternalIPOps = file.MustAssetString("assets/gcp/external-ip.yml")
+	//go:embed assets/gcp/external-ip.yml
+	GCPExternalIPOps string
+
 	// GCPDirectorCustomOps statically defines custom-ops.yml contents
-	GCPDirectorCustomOps = file.MustAssetString("assets/gcp/custom-ops.yml")
+	//go:embed assets/gcp/custom-ops.yml
+	GCPDirectorCustomOps string
+
 	//GCPJumpboxUserOps statically defines gcp jumpbox-user.yml
-	GCPJumpboxUserOps = file.MustAssetString("assets/gcp/jumpbox-user.yml")
+	//go:embed assets/gcp/jumpbox-user.yml
+	GCPJumpboxUserOps string
+
 	// AWSTerraformConfig holds the terraform conf for AWS
-	AWSTerraformConfig = file.MustAssetString("assets/aws/infrastructure.tf")
+	//go:embed assets/aws/infrastructure.tf
+	AWSTerraformConfig string
 
 	// GCPTerraformConfig holds the terraform conf for GCP
-	GCPTerraformConfig = file.MustAssetString("assets/gcp/infrastructure.tf")
+	//go:embed assets/gcp/infrastructure.tf
+	GCPTerraformConfig string
 
 	// AWSReleaseVersions carries all versions of releases
 	AWSReleaseVersions = file.MustAssetString("../../control-tower-ops/ops/versions-aws.json")
@@ -50,13 +68,16 @@ var (
 	GCPReleaseVersions = file.MustAssetString("../../control-tower-ops/ops/versions-gcp.json")
 
 	// AddNewCa carries the ops file that adds a new CA required for cert rotation
-	AddNewCa = file.MustAssetString("assets/maintenance/add-new-ca.yml")
+	//go:embed assets/maintenance/add-new-ca.yml
+	AddNewCa string
 
 	// RemoveOldCa carries the ops file that removes the old CA required for cert rotation
-	RemoveOldCa = file.MustAssetString("assets/maintenance/remove-old-ca.yml")
+	//go:embed assets/maintenance/remove-old-ca.yml
+	RemoveOldCa string
 
 	// CleanupCerts moves renewed values of certs to old keys in director vars store
-	CleanupCerts = file.MustAssetString("assets/maintenance/cleanup-certs.yml")
+	//go:embed assets/maintenance/cleanup-certs.yml
+	CleanupCerts string
 
 	AWSVersionFile = file.MustAsset("../../control-tower-ops/createenv-dependencies-and-cli-versions-aws.json")
 
