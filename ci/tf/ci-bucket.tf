4114ea1f0120782a227a9fc75ac6fad7a561e274
Tom Godkin <tom.godkin@engineerbetter.com>
2021-12-01 10:42:43 +0000

    Format CI terraform

--- ci/tf/ci-bucket.tf
+++ ci/tf/ci-bucket.tf
@@ -1,14 +1,15 @@
 variable "region" {}
-variable "bucket-name" {
-}
+
+variable "bucket-name" {}
 
 provider "aws" {
-  region = "${var.region}"
+  region = var.region
 }
 
 resource "aws_s3_bucket" "ci" {
-  bucket        = "${var.bucket-name}"
-  acl           = "private"
+  bucket = var.bucket-name
+  acl    = "private"
+
   versioning {
     enabled = true
   }
