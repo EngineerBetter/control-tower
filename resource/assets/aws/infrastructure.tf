f79229e50cc86c6f60e257e398aac11251e43d31
Tests <ginkgo@tests.com>
2022-03-15 13:54:32 +0000

    Enable aws rds auto minor upgrades
    This change will let rds manage the minor version. This will avoid
    further issues with unavailable rds versions.
    
    Things to consider:
    - Terraform must told to ignore the minor version
      (=> engine_version = "10")
      If you specifiy a minor version you will eventually end up in trouble,
      because terraform will try to downgrade your DB after an automatic
      minor version upgrade.
    
    - The default version and corresponding upgrades do not sync with
      the postgres releases. The rds team schedules them on a need to
      have basis (e.g. security issues which are relevant for rds).

--- resource/assets/aws/infrastructure.tf
+++ resource/assets/aws/infrastructure.tf
@@ -590,8 +590,8 @@ resource "aws_db_instance" "default" {
   port                        = 5432
   engine                      = "postgres"
   instance_class              = "${var.rds_instance_class}"
-  engine_version              = "10.19"
-  auto_minor_version_upgrade  = false
+  engine_version              = "10"
+  auto_minor_version_upgrade  = true
   allow_major_version_upgrade = true
   name                        = "${var.rds_default_database_name}"
   username                    = "${var.rds_instance_username}"
